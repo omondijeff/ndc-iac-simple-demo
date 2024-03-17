886f9abcbe30dbf34fe07191b43cd1c944cb4b4c
Omondi Jeff <11554jeff@gmail.com>
2024-03-17 18:34:35 +0300

    Terraform Files

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = var.region  # Use the variable for the region
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "ndc-demo-trial"
+ 
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.b.id
+
+  index_document {
+    suffix = "index.html"
+  }
+  
+  error_document {
+    key = "error.html"
+  }
+}
+
+
+
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  description = "The AWS region where resources will be created."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket for the React app."
+  type        = string
+  default     = "ndc-demo"
+}
+
+variable "index_document" {
+  description = "The name of the index document for the website."
+  default     = "index.html"
+}
+
+variable "error_document" {
+  description = "The name of the error document for the website."
+  default     = "error.html"
+}
